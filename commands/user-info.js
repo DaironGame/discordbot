3315250f3bc095be7f06a47e1f14d2f49aaf7371
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 15:25:13 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -1,21 +1,25 @@
-const Discord = module.require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 const moment = require('moment');
 
-module.exports.run = async (bot, message, args) => {
-
-    let user = message.mentions.users.first() || message.author;
-    const joinDiscord = moment(user.createdAt).format('llll');
-    const joinServer = moment(user.joinedAt).format('llll');
-    let embed = new Discord.RichEmbed()
-        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-        .setDescription(`${user}`)
-        .setColor(`RANDOM`)
-        .setThumbnail(`${user.displayAvatarURL}`)
-        .addField('Присоединился:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-        .addField('Статус:', user.presence.status, true)
-        .setFooter(`Айди: ${user.id}`)
-        .setTimestamp();
+module.exports = {
+	name: 'user-info',
+	description: ' Информация о пользователе.',
+	execute(message, args) {
+		let user = message.mentions.users.first() || message.author;
+        const joinDiscord = moment(user.createdAt).format('llll');
+        const joinServer = moment(user.joinedAt).format('llll');
+        let embed = new Discord.RichEmbed()
+         .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
+         .setDescription(`${user}`)
+         .setColor(`RANDOM`)
+         .setThumbnail(`${user.displayAvatarURL}`)
+         .addField('Присоединился:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+         .addField('Статус:', user.presence.status, true)
+         .setFooter(`Айди: ${user.id}`)
+         .setTimestamp();
 
     message.channel.send({ embed: embed });
-    return;
-}
+	},
+};
