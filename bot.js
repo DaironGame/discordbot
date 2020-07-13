2eb239e86e275a0ff1cf9dde399fde1dd3192fac
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 23:15:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,8 @@ client.on('message', message => {
              .setDescription('**Заходи на лучший сервер**\n**бравл старс в майнкрафте!**\n \nАйпи: `daironcraft.xyz`\nВерсия: `1.12.2 optifine`');
             message.channel.send(inviteEmb)
         };
-        let timerId = client.setInterval(noticement(), 5000);
+        var now = new Date(); // Получаем актуальную дату/время
+        client.setTimeout(noticement(), (59 - now.getUTCMinutes()) * 1 * 500);
     };
 
     if (commandName === 'stopnotice') {
