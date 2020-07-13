1ae639bffe2187dcf9d22c6e4257dd0d49fd1151
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 12:47:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
-const { prefix, token } = require('./config.json');
+const { prefix } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
