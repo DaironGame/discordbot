a358f9200e0ae4a892ddf955b158cbb6a8f30b93
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 15:18:55 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -1,19 +1,21 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const Discord = module.require('discord.js');
+const moment = require('moment');
 
-module.exports = {
-	name: 'user-info',
-	description: ' Информация о пользователе.',
-	execute(message, args) {
-		if (!message.mentions.users.size) {
-			return message.channel.send('Укажите пользователя, информацию о котором хотите узнать!');
-		};
+module.exports.run = async (bot, message, args) => {
 
-		let Embed = new Discord.MessageEmbed()
-	          .setColor('#0072FF')
-	          .setTitle('Информация о пользователе')
-                  .setDescription('**В разработке...**')
-                message.channel.send(Embed);
-	},
-};
+    let user = message.mentions.users.first() || message.author;
+    const joinDiscord = moment(user.createdAt).format('llll');
+    const joinServer = moment(user.joinedAt).format('llll');
+    let embed = new Discord.RichEmbed()
+        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
+        .setDescription(`${user}`)
+        .setColor(`RANDOM`)
+        .setThumbnail(`${user.displayAvatarURL}`)
+        .addField('Присоединился:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+        .addField('Статус:', user.presence.status, true)
+        .setFooter(`Айди: ${user.id}`)
+        .setTimestamp();
+
+    message.channel.send({ embed: embed });
+    return;
+}
