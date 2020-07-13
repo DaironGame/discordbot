9b762471b96e56ab35fac73e665917793c3270f1
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 19:41:31 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,21 +6,21 @@ module.exports = {
 	name: 'ban',
 	description: 'Забанить участника.',
 	execute(message, args) {
-	    let xdemb = new Discord.RichEmbed()
+		let xdemb = new Discord.RichEmbed()
          .setColor("#00ff00")
          .setTitle("Комманда бана")
          .addField("Описание:", `Забанить участника`, true)
          .addField("Использование:", `!ban [участник] [причина]`, true)
-         .addField("Пример:", `!ban @OXXSI слив рп`);
+         .addField("Пример:", `!ban @OXXSI слив рп`)
 
-        if (!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== "531116044794855425") return message.channel.send("У вас нет прав!");
+        if(!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== "531116044794855425") return message.channel.send("У вас нет прав!");
 
         let member = message.mentions.members.first();
-        if (!member) return message.channel.send(xdemb);
-        if (!member.bannable) return message.channel.send("I can't ban this user!");
-        if (member.user.id === "531116044794855425") return message.channel.send("Я не могу забанить своего создателя!");
+        if(!member) return message.channel.send(xdemb)
+        if(!member.bannable) return message.channel.send("I can't ban this user!")
+        if(member.user.id === "531116044794855425") return message.channel.send("Я не могу забанить своего создателя!")
 
-        if (member.id === message.author.id) return message.channel.send("Вы не можете забанить самого себя");
+        if(member.id === message.author.id) return message.channel.send("Вы не можете забанить самого себя")
 
         let reason = args.slice(1).join(" ");
 
@@ -28,21 +28,21 @@ module.exports = {
             res = "Отсутствует причина";
         } else {
             res = reason
-        };
+        }
 
         await member.ban(reason).catch(error => message.channel.send(`Изините, я не могу забанить потому что: ${error}`));
 
         let bean = new Discord.RichEmbed()
          .setColor("#00ff00")
-         .setTitle(`Бан | ${member.user.tag}`)
-         .addField("Участник", member, true)
-         .addField("Модератор", message.author)
-         .addField("Причина", res)
+         .setTitle(`Ban | ${member.user.tag}`)
+         .addField("User", member, true)
+         .addField("Moderator", message.author, true)
+         .addField("Reason", res)
          .setTimestamp();
 
-        message.channel.send(bean);
+        message.channel.send(bean)
 
-        message.delete();
+        message.delete()
 
 	},
 };
