0c60f81343b6ab836e11bd0312a80baad23e800d
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 16:06:02 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -5,17 +5,17 @@ module.exports = {
 	name: 'user-info',
 	description: ' Информация о пользователе.',
 	execute(message, args) {
-	 let ment = message.mentions.users.first();
-		if(!ment) {
-			ment = message.author;
-		},
-	// Creats an embed with information about the mentioned user
-		let embed = new Discord.RichEmbed()
-		.addField("Username", ment.tag)
-		.addField("ID", ment.id)
-		.addField("Status", ment.presence.status)
-		.addField("Created", ment.createdAt)
-		.setThumbnail(ment.avatarURL);
-		message.channel.send(embed);
+// 	 let ment = message.mentions.users.first();
+// 		if(!ment) {
+// 			ment = message.author;
+// 		},
+// 	// Creats an embed with information about the mentioned user
+// 		let embed = new Discord.RichEmbed()
+// 		.addField("Username", ment.tag)
+// 		.addField("ID", ment.id)
+// 		.addField("Status", ment.presence.status)
+// 		.addField("Created", ment.createdAt)
+// 		.setThumbnail(ment.avatarURL);
+// 		message.channel.send(embed);
 	},
 };
