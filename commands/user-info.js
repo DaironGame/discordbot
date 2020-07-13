0fc694317b0edf0a43133d2e2d538b7503959c31
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 15:32:40 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
 const moment = require('moment');
 
 module.exports = {
 	name: 'user-info',
 	description: ' Информация о пользователе.',
 	execute(message, args) {
-		let user = message.mentions.users.first() || message.author;
+	let user = message.mentions.users.first() || message.author;
         const joinDiscord = moment(user.createdAt).format('llll');
         const joinServer = moment(user.joinedAt).format('llll');
         let embed = new Discord.RichEmbed()
@@ -17,8 +15,7 @@ module.exports = {
          .setThumbnail(`${user.displayAvatarURL}`)
          .addField('Присоединился:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
          .addField('Статус:', user.presence.status, true)
-         .setFooter(`Айди: ${user.id}`)
-         .setTimestamp();
+         .setFooter(`Айди: ${user.id}`);
 
     message.channel.send({ embed: embed });
 	},
