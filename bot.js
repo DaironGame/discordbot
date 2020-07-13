f3bae4b4430b0efe5483a22eb554d926c43eeccc
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 14:12:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-// const fs = require('fs');
+const fs = require('fs');
 
 client.commands = new Discord.Collection();
 // const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
