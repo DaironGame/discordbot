1f75dea7298f164a08a2a1920bdcb22f3dedc07f
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 18:06:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
 const prefix = ".";
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -18,35 +20,30 @@ client.on("ready", () => {
    channel.send(`Бот запущен!`);
 });
 
-//запрещенные слова
-let notAllowedWords = new Array("сука", "пидор", "блять", "хуила", "хуй","мудила","шлюха","гей","чсв","бля","ска","cука",
-"сюк","блет","блэт","гавно", "ахуеть","ахуел","чмо","пидр","дебил","даун","заебал","сук","соси","пососи","сосать", "жопа",
-"сюк", "чмошник", "пох", "похуй", "тварь", "лох", "еблан", "хуйня","пидар", "пидарасина");
+// //запрещенные слова
+// let notAllowedWords = new Array("сука", "пидор", "блять", "хуила", "хуй","мудила","шлюха","гей","чсв","бля","ска","cука",
+// "сюк","блет","блэт","гавно", "ахуеть","ахуел","чмо","пидр","дебил","даун","заебал","сук","соси","пососи","сосать", "жопа",
+// "сюк", "чмошник", "пох", "похуй", "тварь", "лох", "еблан", "хуйня","пидар", "пидарасина");
 
 
 client.on('message', message => {
-
-     //антимат
-    let arrayOfStrings = message.content.toLowerCase().split(' ');
-    for (mat of notAllowedWords) {
-        if (arrayOfStrings.includes(mat)) {
-            message.delete();
-            message.reply("такое говорить запрещено!");
-            return;
-        };
-    };
+//      //антимат
+//     let arrayOfStrings = message.content.toLowerCase().split(' ');
+//     for (mat of notAllowedWords) {
+//         if (arrayOfStrings.includes(mat)) {
+//             message.delete();
+//             message.reply("такое говорить запрещено!");
+//             return;
+//         };
+//     };
 
     //проверка на комманду и т.п.
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;  
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;  
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
 	
-    //restart
-    if (commandName === 'mp') {
-        process.exit();
-    };
 
     //say
     if (commandName === 'say') {
