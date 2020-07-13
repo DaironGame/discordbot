f64cca00e5dfa118da8c28e595b2397c0e5b738a
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 15:34:13 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -5,18 +5,17 @@ module.exports = {
 	name: 'user-info',
 	description: ' Информация о пользователе.',
 	execute(message, args) {
-	let user = message.mentions.users.first() || message.author;
-        const joinDiscord = moment(user.createdAt).format('llll');
-        const joinServer = moment(user.joinedAt).format('llll');
-        let embed = new Discord.RichEmbed()
-         .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-         .setDescription(`${user}`)
-         .setColor(`RANDOM`)
-         .setThumbnail(`${user.displayAvatarURL}`)
-         .addField('Присоединился:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-         .addField('Статус:', user.presence.status, true)
-         .setFooter(`Айди: ${user.id}`);
-
-    message.channel.send({ embed: embed });
+	 let user = message.mentions.users.first() || message.author;
+         const joinDiscord = moment(user.createdAt).format('llll');
+         const joinServer = moment(user.joinedAt).format('llll');
+         let embed = new Discord.RichEmbed()
+          .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
+          .setDescription(`${user}`)
+          .setColor(`RANDOM`)
+          .setThumbnail(`${user.displayAvatarURL}`)
+          .addField('Присоединился:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+          .addField('Статус:', user.presence.status, true)
+          .setFooter(`Айди: ${user.id}`);
+         message.channel.send(embed);
 	},
 };
