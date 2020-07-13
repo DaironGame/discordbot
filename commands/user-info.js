b97660ecc02c5c3c47eec50ba1105d881d91ac71
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-13 15:44:29 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -5,17 +5,17 @@ module.exports = {
 	name: 'user-info',
 	description: ' Информация о пользователе.',
 	execute(message, args) {
-	 let user = message.mentions.users.first() || message.author;
-         const joinDiscord = moment(user.createdAt).format('llll');
-         const joinServer = moment(user.joinedAt).format('llll');
-         let embed = new Discord.RichEmbed()
-          .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-          .setDescription(`${user}`)
-          .setColor(`RANDOM`)
-          .setThumbnail(`${user.displayAvatarURL}`)
-          .addField('Присоединился:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-          .addField('Статус:', user.presence.status, true)
-          .setFooter(`Айди: ${user.id}`);
-         message.channel.send(embed);
+	 let ment = message.mentions.users.first();
+		if(!ment) {
+			ment = message.author;
+		},
+	// Creats an embed with information about the mentioned user
+		let embed = new Discord.RichEmbed()
+		.addField("Username", ment.tag)
+		.addField("ID", ment.id)
+		.addField("Status", ment.presence.status)
+		.addField("Created", ment.createdAt)
+		.setThumbnail(ment.avatarURL);
+		message.channel.send(embed);
 	},
 };
