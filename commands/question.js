06829d5b7085f57cf2a4102e699e25a1e315c14f
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 15:53:33 +0300

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'Погадать.',
 	execute(message, args) {
         let ranColor = "#" + Math.random().toString(16).slice(2, 8);
-        let answers = new Array("Сейчас не могу предсказать :eyes:", "Знаки говорят — да :ok_hand:", "Спроси позже :eyes:", "Мой ответ — нет :broken_heart:", "Мне кажется — да :ok_hand:", "Даже не думай :x:", "Можешь быть уверен в этом :thumbsup:", "Сконцентрируйся и спроси еще разок :eyes:")
+        let answers = new Array("Сейчас не могу предсказать :eyes:", "Знаки говорят — да :ok_hand:", "Спроси позже :eyes:", "Мой ответ — нет :broken_heart:", "Мне кажется — да :ok_hand:", "Даже не думай :x:", "Можешь быть уверен в этом :thumbsup:", "Сконцентрируйся и спроси еще разок :eyes:");
         if (!args[0]) {
 		 let qEmbed = new Discord.MessageEmbed()
 	      .setColor(ranColor)
