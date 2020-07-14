5c6b119e76ef77fa5517db317440f07afcb97042
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 15:47:45 +0300

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -16,10 +16,9 @@ module.exports = {
         } else {
             let answer = answers[Math.floor(Math.random() * rand_1.length)];
             let sEmbed = new Discord.MessageEmbed()
-             .setColor(ranColor)
-             .setTitle('Шар')
-             .setDescription(`Все ответы не настоящие, просьба относится к ответам как к шутке, \nи не верить всему сказанному в данном сообщении.`)
-             .addField(args.slice(1).join(" "), answer, true);
+            .setColor(ranColor)
+            .setTitle('Шар')
+            .setDescription(`Все ответы не настоящие, просьба относится к ответам как к шутке, \nи не верить всему сказанному в данном сообщении.\n \n${args.slice(1).join(" ")}\n${answer}`);
            message.channel.send(sEmbed);
         };
 	
