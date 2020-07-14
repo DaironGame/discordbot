8b5737243fe339c045fb1a35537596793c3f538c
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 19:53:45 +0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -13,22 +13,32 @@ module.exports = {
          .setColor('#008BD7')
 	     .setTitle('Мут')
 		 .setDescription('**Использование:**')
-		 .addField('.mute [ник] [время]', 'Например: `.mute @OXSSI 10h`', true);
+         .addField('.mute [ник] [время]', 'Например: `.mute @OXSSI 10h`', true)
+         .setFooter('DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png');
         let tomute = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
         if(!tomute) return message.channel.send(howUse);
-        if(message.author.id === message.mentions.users.first()) return message.reply("ты не можешь замутить самого себя :facepalm-1:");
         let muteRole = message.guild.roles.cache.find(val => val.name === "Muted");
 
     let mutetime = args[1];
     if(!mutetime) return message.channel.send(howUse);
+
+    let reason = args.slice(1).join(" ");
+
+        if(!reason) {
+            res = "не указана";
+        } else {
+            res = reason
+        };
     
     let embedd = new Discord.MessageEmbed()
      .setColor('#008BD7')
+     .setTitle('Мут')
      .setTimestamp()
      .addField('Замученный:', `${tomute}`)
      .addField('Замутивший:', `${message.author}`)
-     .addField('Время мута', ms(ms(mutetime)))
-     .setFooter(`Dairon Chat`);
+     .addField('Время мута', ms(ms(mutetime)), true)
+     .addField('Причина', res, true)
+     .setFooter('DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png');
     message.channel.send(embedd);
 
     message.guild.member(tomute).roles.add(muteRole);
