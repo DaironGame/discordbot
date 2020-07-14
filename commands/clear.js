23f0f472a7a96068b8756a66244d5c0f29bcac96
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 11:26:46 +0300

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'clear',
+	description: 'Очистка.',
+	execute(message, args) {
+        const amount = parseInt(args[0]);
+
+	    if (isNaN(amount)) {
+		 return message.channel.send('Укажите число.');
+	    } else if (amount < 1 || amount > 100) {
+            return message.channel.send('Введите число от 1 до 100.');
+        };
+        message.channel.bulkDelete(amount, true);
+
+	},
+};
