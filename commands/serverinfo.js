c9b5d4bbcacdb7be263e39500a443e4938042a2f
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 12:39:13 +0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -32,18 +32,21 @@ module.exports = {
                 "russia": ":flag_ru: Russia"
             };
             const embed = new Discord.MessageEmbed()
-                .setAuthor(message.guild.name, message.guild.iconURL);
-                // .addField("Name", message.guild.name, true)
-                // .addField("ID", message.guild.id, true)
-                // .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
-                // .addField("Region", region[message.guild.region], true)
-                // .addField("Total | Humans | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`, true)
-                // .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
-                // .addField("Channels", message.guild.channels.size, true)
-                // .addField("Roles", message.guild.roles.size, true)
-                // .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
-                // .setThumbnail(message.guild.iconURL)
-            message.channel.send({embed});
+                .setAuthor(message.guild.name, message.guild.iconURL)
+                .setDescription(' ')
+                .addFields(
+                    { name: 'Название', value: message.guild.name, inline: true },
+                    { name: 'Айди', value: message.guild.id, inline: true },
+                    { name: 'Создатель', value: message.guild.owner, inline: true },
+                    { name: 'Регион', value: region[message.guild.region], inline: true },
+                    { name: 'Всего | Людей | Ботов', value: `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`, inline: true },
+                    { name: 'Уровень модерации', value: verifLevels[message.guild.verificationLevel], inline: true },
+                    { name: 'Каналы', value: message.guild.channels.size, inline: true },
+                    { name: 'Роли', value: message.guild.roles.size, inline: true },
+                    { name: 'Дата создания', value: `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, inline: true },
+                )
+                .setThumbnail(message.guild.iconURL);
+            message.channel.send(embed);
 
 	},
 };
