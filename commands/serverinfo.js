c627ae6e532d4969e5ece2ceb32cb189416ef2a7
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 11:49:24 +0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -18,16 +18,16 @@ module.exports = {
             .setColor("#00ff00")
             .setThumbnail(sicon)
             .setAuthor(message.guild.name)
-            .addField("Имя", message.guild.name, inline)
-            .addField("Айди", message.guild.id, inline)
-            .addField("Создатель", message.guild.owner, inline)
-            .addField("Регион", message.guild.region, inline)
-            .addField("Уровень модерации", verlvl[message.guild.verificationLevel],inline)
-            .addField("Участники", `<:user:531116044794855425> ${message.guild.memberCount}`, inline)
-            .addField("Роли", message.guild.roles.size, inline)
-            .addField("Каналы", message.guild.channels.size, inline)
-            .addField("Вы присоединились", message.member.joinedAt)
-            .setFooter(`Создан ${message.guild.createdAt}`);
+//             .addField("Имя", message.guild.name, inline)
+//             .addField("Айди", message.guild.id, inline)
+//             .addField("Создатель", message.guild.owner, inline)
+//             .addField("Регион", message.guild.region, inline)
+//             .addField("Уровень модерации", verlvl[message.guild.verificationLevel],inline)
+//             .addField("Участники", `<:user:531116044794855425> ${message.guild.memberCount}`, inline)
+//             .addField("Роли", message.guild.roles.size, inline)
+//             .addField("Каналы", message.guild.channels.size, inline)
+//             .addField("Вы присоединились", message.member.joinedAt)
+//             .setFooter(`Создан ${message.guild.createdAt}`);
         
             message.channel.send(serverembed);
         
