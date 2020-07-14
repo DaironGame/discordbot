835b74a67d3fcfe4a3a037aa494c2d985cafca65
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 12:59:04 +0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,23 +4,6 @@ module.exports = {
 	name: 'serverinfo',
 	description: 'Инфа о сервере.',
 	execute(message, args) {
-            // let serverEmbed = new Discord.MessageEmbed()
-            // .setColor("#00ff00")
-            // .setThumbnail(message.guild.iconURL)
-            // .setAuthor(message.guild.name)
-            // .addFields(
-            //     { name: 'Название', value: message.guild.name, inline: true },
-            //     { name: 'Айди', value: message.guild.id, inline: true },
-            //     { name: 'Создатель', value: message.guild.owner, inline: true },
-            //     { name: 'Регион', value: message.guild.region, inline: true },
-            //     { name: 'Участники', value: `<:user:531116044794855425> ${message.guild.memberCount}`, inline: true },
-            //     { name: 'Роли', value: message.guild.roles.size, inline: true },
-            //     { name: 'Вы присоединились', value: message.member.joinedAt, inline: false },
-            // )
-            // .setFooter(`Создан ${message.guild.createdAt}`);
-        
-            // message.channel.send(serverEmbed);
-
             function checkDays(date) {
                 let now = new Date();
                 let diff = now.getTime() - date.getTime();
@@ -30,7 +13,7 @@ module.exports = {
             let region = {
                 "russia": ":flag_ru: Russia"
             };
-            const embed = new Discord.MessageEmbed()
+            let servembed = new Discord.MessageEmbed()
                 .setColor('#00ff00')
                 .setAuthor(message.guild.name, message.guild.iconURL)
                 .setThumbnail(message.guild.iconURL)
@@ -42,7 +25,7 @@ module.exports = {
                     { name: 'Роли', value: message.guild.roles.size, inline: true },
                     { name: 'Дата создания', value: `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, inline: true },
                 );
-            message.channel.send(embed);
+            message.channel.send(servembed);
 
 	},
 };
