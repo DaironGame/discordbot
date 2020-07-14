775f93786cafd75900df2224629a9035469806dd
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 12:23:28 +0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -32,17 +32,17 @@ module.exports = {
                 "russia": ":flag_ru: Russia"
             };
             const embed = new Discord.MessageEmbed()
-                .setAuthor(message.guild.name, message.guild.iconURL)
-                .addField("Name", message.guild.name, true)
-                .addField("ID", message.guild.id, true)
-                .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
-                .addField("Region", region[message.guild.region], true)
-                .addField("Total | Humans | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`, true)
-                .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
-                .addField("Channels", message.guild.channels.size, true)
-                .addField("Roles", message.guild.roles.size, true)
-                .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
-                .setThumbnail(message.guild.iconURL)
+                .setAuthor(message.guild.name, message.guild.iconURL);
+                // .addField("Name", message.guild.name, true)
+                // .addField("ID", message.guild.id, true)
+                // .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
+                // .addField("Region", region[message.guild.region], true)
+                // .addField("Total | Humans | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`, true)
+                // .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
+                // .addField("Channels", message.guild.channels.size, true)
+                // .addField("Roles", message.guild.roles.size, true)
+                // .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
+                // .setThumbnail(message.guild.iconURL)
             message.channel.send({embed});
 
 	},
