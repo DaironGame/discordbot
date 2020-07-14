38fad45b72d2273b089f0c781be99c5183818d01
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 13:28:05 +0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,21 +4,12 @@ module.exports = {
 	name: 'serverinfo',
 	description: 'Инфа о сервере.',
 	execute(message, args) {
-            function checkDays(date) {
-                let now = new Date();
-                let diff = now.getTime() - date.getTime();
-                let days = Math.floor(diff / 86400000);
-                return days + (days == 1 ? " day" : " days") + " ago";
-            };
-            let region = {
-                "russia": ":flag_ru: Russia"
-            };
             let servembed = new Discord.MessageEmbed()
                 .setColor('#00ff00')
                 .setAuthor(message.guild.name, message.guild.iconURL)
                 .setThumbnail(message.guild.iconURL)
                 .setTitle('Информация о сервере')
-                .setDescription(`Название: ${message.guild.name}\nАйди: ${message.guild.id}\nСоздатель: ${message.guild.owner}\nРегион: ${region[message.guild.region]}\nВсего | Людей | Ботов\n${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}\nКаналы: ${message.guild.channels.size}\nРоли: ${message.guild.roles.size}`);
+                .setDescription(`Название: ${message.guild.name}\nАйди: ${message.guild.id}\nСоздатель: ${message.guild.owner}\nРегион: :flag_ru: Россия\nКаналы: ${message.guild.channels.size}\nРоли: ${message.guild.roles.size}`);
             message.channel.send(servembed);
 
 	},
