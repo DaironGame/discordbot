fee4fe1038fc41db6c6e2a828eb6fd2bb14aa0fb
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 11:18:14 +0300

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -4,18 +4,18 @@ module.exports = {
 	name: 'question',
 	description: 'Погадать.',
 	execute(message, args) {
-    //     let ranColor = "#" + Math.random().toString(16).slice(2, 8);
-    //     if (args === '') {
-	// 	message.reply('1');
-	// 	let Embed = new Discord.MessageEmbed()
-	//          .setColor(ranColor)
-	//          .setTitle('Вопрос')
-	// 	 .setDescription('**Использование:**')
-	// 	 .addField('.question [вопрос]', 'Например: `.question сегодня выйдет видео у Аида?`', true);
-	// 	message.channel.send(Embed);
-    //     } else {
-	// message.reply('2');
-    // };
+        let ranColor = "#" + Math.random().toString(16).slice(2, 8);
+        if (args === '' || args == ' ' || !args) {
+		message.reply('1');
+		let Embed = new Discord.MessageEmbed()
+	         .setColor(ranColor)
+	         .setTitle('Вопрос')
+		 .setDescription('**Использование:**')
+		 .addField('.question [вопрос]', 'Например: `.question сегодня выйдет видео у Аида?`', true);
+		message.channel.send(Embed);
+        } else {
+	message.reply('2');
+    };
     message.reply(`${message} - ${args}`)
 	
 
