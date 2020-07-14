0bb07f6bedde8d26114cc9ab6787080b6472969f
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 12:05:37 +0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,11 +4,11 @@ module.exports = {
 	name: 'serverinfo',
 	description: 'Инфа о сервере.',
 	execute(message, args) {
-            // let inline = true
-            // let serverEmbed = new Discord.MessageEmbed()
-            // .setColor("#00ff00")
-            // .setThumbnail(message.guild.iconURL)
-            // .setAuthor(message.guild.name);
+            let inline = true
+            let serverEmbed = new Discord.MessageEmbed()
+            .setColor("#00ff00")
+            .setThumbnail(message.guild.iconURL)
+            .setAuthor(message.guild.name);
 //             .addField("Имя", message.guild.name, inline)
 //             .addField("Айди", message.guild.id, inline)
 //             .addField("Создатель", message.guild.owner, inline)
@@ -20,7 +20,7 @@ module.exports = {
 //             .addField("Вы присоединились", message.member.joinedAt)
 //             .setFooter(`Создан ${message.guild.createdAt}`);
         
-            // message.channel.send(serverEmbed);
+            message.channel.send(serverEmbed);
         
             message.delete();
 
