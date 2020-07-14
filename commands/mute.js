cd1a6aec30d8d9c3c5850f5e0dcd44c6313f78b4
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-14 19:09:26 +0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -6,13 +6,13 @@ module.exports = {
 	description: 'мут на время.',
 	execute(message, args) {
         let tomute = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
-    if(!tomute) return message.reply("не получилось найти пользователя.");
-    if(message.author.id === message.mentions.users.first()) return message.reply("ты не можешь замутить самого себя :facepalm-1:");
-    let muteRole = message.guild.roles.cache.find(val => val.name === "Мут");
-    if (!muteRole) {
-        try {
+        if(!tomute) return message.reply("не получилось найти пользователя.");
+        if(message.author.id === message.mentions.users.first()) return message.reply("ты не можешь замутить самого себя :facepalm-1:");
+        let muteRole = message.guild.roles.cache.find(val => val.name === "Muted");
+        if (!muteRole) {
+         try {
             muteRole = await message.guild.roles.create({data:{
-                name:"Мут",
+                name:"Muted",
                 color: "#000000",
                 permissions:[]
             }});
@@ -25,21 +25,21 @@ module.exports = {
                     ADD_REACTIONS: false
                 });
             });
-        } catch(e) {
+         } catch(e) {
             console.log(e.stack);
-        }
-    }
+         }
+        };
     let mutetime = args[1];
     if(!mutetime) return message.reply("вы не указали время для мута.");
     
-    const embed = new Discord.MessageEmbed()
+    const embedd = new Discord.MessageEmbed()
     .setColor(0x00FFFF)
     .setTimestamp()
     .addField('Замученный:', `${tomute.username}#${tomute.discriminator} (${tomute.id})`)
     .addField('Замутивший:', `${message.author.username}#${message.author.discriminator}`)
     .addField('Время мута', ms(ms(mutetime)))
     .setFooter(`Dairon Chat`);
-    message.channel.send({embed});
+    message.channel.send(embedd);
 
     message.guild.member(tomute).roles.add(muteRole);
 
