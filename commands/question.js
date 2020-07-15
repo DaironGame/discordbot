ca55ef3ff01d36751f66f56df08dcd4660964c6f
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-15 16:31:37 +0300

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 
 module.exports = {
 	name: 'question',
-	description: 'Погадать.',
+        description: 'Погадать.',
+        aliases: ['q', 'шар'],
 	execute(message, args) {
         let ranColor = "#" + Math.random().toString(16).slice(2, 8);
         let answers = new Array("Сейчас не могу предсказать :eyes:", "Знаки говорят — да :ok_hand:", "Спроси позже :eyes:", "Мой ответ — нет :broken_heart:", "Мне кажется — да :ok_hand:", "Даже не думай :x:", "Можешь быть уверен в этом :thumbsup:", "Сконцентрируйся и спроси еще разок :eyes:");
