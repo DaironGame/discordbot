143a8a4d2db5af91b86cc33cbe5f7deba7c296d7
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-15 14:59:32 +0300

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,16 +2,33 @@ const Discord = require('discord.js');
 const superagent = require('superagent');
 const sf = require("snekfetch");
 
-exports.run = async (client, message, args) => {
-    const { body } = await superagent
-    .get("http://aws.random.cat/meow");
+// exports.run = async (client, message, args) => {
+//     const { body } = await superagent
+//     .get("http://aws.random.cat/meow");
 
-    const embed = new Discord.MessageEmbed()
-    .setColor("#ff9900")
-    .setTitle("Вот твой котик")
-    .setImage(body.file) 
-    .setFooter(`DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png`);
-    message.channel.send({embed});
-};
+//     const embed = new Discord.MessageEmbed()
+//     .setColor("#ff9900")
+//     .setTitle("Вот твой котик")
+//     .setImage(body.file) 
+//     .setFooter(`DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png`);
+//     message.channel.send({embed});
+// };
 //    aliases: [],
+
+module.exports = {
+	name: 'cat',
+	description: 'cats.',
+	execute(message, args) {
+        const { body } = await superagent
+         .get("http://aws.random.cat/meow");
+            
+        const embed = new Discord.MessageEmbed()
+         .setColor("#ff9900")
+         .setTitle("Вот твой котик")
+         .setImage(body.file) 
+         .setFooter(`DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png`);
+        message.channel.send({embed});
+
+	},
+};
    
