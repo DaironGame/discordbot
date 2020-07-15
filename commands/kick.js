2b24a5231ee456c9a9b5408e227d99888ca0be4b
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-15 19:03:47 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,28 +4,28 @@ module.exports = {
 	name: 'kick',
 	description: 'kickk.',
 	execute(message, args) {
-        if (!message.member.hasPermission("KICK_MEMBERS")) {
-            message.channel.send("У вас нет прав!");
-            return;
-        };
+//         if (!message.member.hasPermission("KICK_MEMBERS")) {
+//             message.channel.send("У вас нет прав!");
+//             return;
+//         };
 		
-        let reason = args.slice(1).join(' ');
-        let howUse = new Discord.MessageEmbed()
-         .setColor('#8D00D7')
-	     .setTitle('Кик')
-		 .setDescription('**Использование:**')
-         .addField('.kick [ник] [причина]', 'Например: `.kick @OXSSI оскорбление администрации`', true)
-         .setFooter('DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png');
-        let user = message.mentions.users.first();
-        if (message.mentions.users.size < 1) return message.channel.send(howUse);
-        if (user.id === message.author.id) return message.reply("я не могу позволить тебе сделать это, суицид - не выход! :facepalm-1:");
-        if (user.id === client.user.id) return message.reply("ты думаешь, что я кикну самого себя?) :joy:");
+//         let reason = args.slice(1).join(' ');
+//         let howUse = new Discord.MessageEmbed()
+//          .setColor('#8D00D7')
+// 	     .setTitle('Кик')
+// 		 .setDescription('**Использование:**')
+//          .addField('.kick [ник] [причина]', 'Например: `.kick @OXSSI оскорбление администрации`', true)
+//          .setFooter('DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png');
+//         let user = message.mentions.users.first();
+//         if (message.mentions.users.size < 1) return message.channel.send(howUse);
+//         if (user.id === message.author.id) return message.reply("я не могу позволить тебе сделать это, суицид - не выход! :facepalm-1:");
+//         if (user.id === client.user.id) return message.reply("ты думаешь, что я кикну самого себя?) :joy:");
         
-        if(!reason) {
-            res = "не указана";
-        } else {
-            res = reason
-        };
+//         if(!reason) {
+//             res = "не указана";
+//         } else {
+//             res = reason
+//         };
         
 //         let embedd = new Discord.MessageEmbed()
 //          .setColor('#8D00D7')
