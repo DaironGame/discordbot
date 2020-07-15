0d84a2cad2feafb04395c869117f06242b7315fe
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-15 16:10:58 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 
 module.exports = {
 	name: 'avatar',
-	description: 'ava.',
+    description: 'ava.',
+    aliases: ['ava'],
 	execute(message, args) {
         if (message.mentions.users.first() == '531116044794855425' || message.mentions.users.first() == '694281589961654292') {
             message.channel.send('Аватар данного пользователя заблокирован.');
