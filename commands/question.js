22ff3098fca24a375d6efa8bb0810bcee67213e5
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-15 22:33:56 +0300

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -6,7 +6,13 @@ module.exports = {
         aliases: ['q', 'шар'],
 	execute(message, args) {
         let ranColor = "#" + Math.random().toString(16).slice(2, 8);
-        let answers = new Array("Сейчас не могу предсказать :eyes:", "Знаки говорят — да :ok_hand:", "Спроси позже :eyes:", "Мой ответ — нет :broken_heart:", "Мне кажется — да :ok_hand:", "Даже не думай :x:", "Можешь быть уверен в этом :thumbsup:", "Сконцентрируйся и спроси еще разок :eyes:");
+        let notAllow = new Array("дайрон", "даирон", "дарон", "дирон", "дэйрон", "дэирон", "дейрон", "деирон",
+        "дайран", "даиран", "окси", "краш", "крашнет", "dairon", "dauron", "дaйрон", "дaирон", "дaрон", "дaйрaн", "дaирaн",
+        "дайрaн", "dаiron", "dairоn", "дайрoн", "oкси", "окcи", "oкcи");
+        let answers = new Array("Сейчас не могу предсказать :eyes:", "Знаки говорят — да :ok_hand:", "Спроси позже :eyes:",
+        "Мой ответ — нет :broken_heart:", "Мне кажется — да :ok_hand:", "Даже не думай :x:", "Можешь быть уверен в этом :thumbsup:",
+        "Сконцентрируйся и спроси еще разок :eyes:","Не могу ответить :takssss:", ":izidi:", "Сегодня не предсказываю :thanks_boomer:",
+        "Еще раз спросишь :ban:ан получишь :frog4:", "Скорее всего... СЛИШКОМ СЛОЖНЫЙ ВОПРОС :zapor:", ":tidebil: Тут же все очевидно!");
         if (!args[0]) {
 		 let qEmbed = new Discord.MessageEmbed()
 	      .setColor(ranColor)
@@ -17,6 +23,9 @@ module.exports = {
 		 message.channel.send(qEmbed);
         } else {
             let answer = answers[Math.floor(Math.random() * answers.length)];
+            for (nall of notAllow) {
+                if (message.toString.includes(nall)) answer = "Не могу ответить :takssss:"
+            };
             let sEmbed = new Discord.MessageEmbed()
             .setColor(ranColor)
             .setTitle('Шар')
