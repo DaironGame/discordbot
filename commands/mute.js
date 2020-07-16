e75201131cc7411a2f593f4d358b2ca8a360c31c
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-16 19:24:59 +0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -24,6 +24,7 @@ module.exports = {
     if(!mutetime) return message.channel.send(howUse);
 
     let reason = args.slice(2).join(" ");
+    let res;
 
         if(!reason) {
             res = "не указана";
