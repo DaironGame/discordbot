8cc9a6df527901b802a70e1524104fa0b5daedf6
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-17 18:43:05 +0300

    Update sendem.js

--- commands/sendem.js
+++ commands/sendem.js
@@ -9,16 +9,15 @@ module.exports = {
             let yt = new Discord.MessageEmbed()
              .setColor("16711680")
              .setTitle("Реклама")
-             .setDescription("Реклама на канале **Dairon Game**")
              .setURL("https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g")
-             .setAuthor("", "https://cdn.discordapp.com/attachments/730419733672886280/733684331922456677/download_1.png", "")
+             .setDescription("Реклама на канале **Dairon Game**")
              .setThumbnail("https://pngimg.com/uploads/youtube/youtube_PNG2.png")
-             .setFooter("DaironChat", "https://cdn.discordapp.com/attachments/730419733672886280/733689040733208606/pack.png")
              .addFields(
                 { name: 'Реклама в видео', value: '**100 руб**\n**Приход:** 100 - 200 подписчиков в среднем.' },
                 { name: 'Реклама в сообществе (на ютубе)', value: '**50 руб**\n**Приход:** зависит от качества поста.' },
                 { name: '**Сроки:**', value: '**Реклама в сообществе** будет опубликована\nчерез несколько минут после оплаты, или\nкогда вы пожелаете.\n**Реклама в видео** будет в видео про\nрп/карту/аддон и т.п.\nИсключения: пиар в в видео может быть чуть позже\nиз-за очереди или если рп будет слит просто так (редко)' },
-            )
+             )
+             .setFooter("DaironChat", "https://cdn.discordapp.com/attachments/730419733672886280/733684331922456677/download_1.png")
          message.channel.send(yt);
         } else if (args[0] === 'ds') {
             message.channel.send("ds")
