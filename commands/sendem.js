30eee9a626e136514267a327258f67e655ae1e1f
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-17 19:00:18 +0300

    Update sendem.js

--- commands/sendem.js
+++ commands/sendem.js
@@ -5,55 +5,44 @@ module.exports = {
 	description: 'Помощь.',
 	execute(message, args) {
         if (args[0] === 'ds') {
-            // let yt = new Discord.MessageEmbed()
-            //  .setColor("FF0000")
-            //  .setTitle("Реклама")
-            //  .setURL("https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g")
-            //  .setDescription("Реклама на канале **Dairon Game**")
-            //  .setThumbnail("https://pngimg.com/uploads/youtube/youtube_PNG2.png")
-            //  .addFields(
-            //     { name: 'Реклама в видео', value: '**100 руб**\n**Приход:** 100 - 200 подписчиков в среднем.' },
-            //     { name: 'Реклама в сообществе (на ютубе)', value: '**50 руб**\n**Приход:** зависит от качества поста.' },
-            //     { name: '**Сроки:**', value: '**Реклама в сообществе** будет опубликована\nчерез несколько минут после оплаты, или\nкогда вы пожелаете.\n**Реклама в видео** будет в видео про\nрп/карту/аддон и т.п.\nИсключения: пиар в в видео может быть чуть позже\nиз-за очереди или если рп будет слит просто так (редко)' },
-            //  )
-            //  .setFooter("DaironChat", "https://cdn.discordapp.com/attachments/730419733672886280/733684331922456677/download_1.png")
             let ds = {
                 "title": "Реклама",
-    "description": "Реклама в данном дискорд сервере в канале **новости**",
-    "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
-    "color": "FF0000",
-    "footer": {
-      "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733689040733208606/pack.png",
-      "text": "DaironChat"
-    },
-    "thumbnail": {
-      "url": "https://lh3.googleusercontent.com/_4zBNFjA8S9yjNB_ONwqBvxTvyXYdC7Nh1jYZ2x6YEcldBr2fyijdjM2J5EoVdTpnkA"
-    },
-    "author": {
-      "name": "Dairon",
-      "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
-      "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733684331922456677/download_1.png"
-    },
-    "fields": [
-      {
-        "name": "Без пинга",
-        "value": "**10 руб**\n"
-      },
-      {
-        "name": "С массовым пингом @here",
-        "value": "**30 руб**\n"
-      },
-      {
-        "name": "С массовым пингом @everyone",
-        "value": "**50 руб**\n"
-      },
-      {
-        "name": "Сроки",
-        "value": "Когда вы пожелаете, хоть через минуту после оплаты"
-      }
-    ]
+                "description": "Реклама в данном дискорд сервере в канале **новости**",
+                "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
+                "color": "FF0000",
+                "footer": {
+                "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733689040733208606/pack.png",
+                "text": "DaironChat"
+                },
+                "thumbnail": {
+                "url": "https://lh3.googleusercontent.com/_4zBNFjA8S9yjNB_ONwqBvxTvyXYdC7Nh1jYZ2x6YEcldBr2fyijdjM2J5EoVdTpnkA"
+                },
+                "author": {
+                "name": "Dairon",
+                "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
+                "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733684331922456677/download_1.png"
+                },
+                "fields": [
+                {
+                    "name": "Без пинга",
+                    "value": "**10 руб**\n"
+                },
+                {
+                    "name": "С массовым пингом @here",
+                    "value": "**30 руб**\n"
+                },
+                {
+                    "name": "С массовым пингом @everyone",
+                    "value": "**50 руб**\n"
+                },
+                {
+                    "name": "Сроки",
+                    "value": "Когда вы пожелаете, хоть через минуту после оплаты"
+                }
+                ]
             }
-            client.channels.cache.get('697352322250178570').send({ embed: ds })
+        message.channel.send({ embed: ds });
+        message.delete();
         } else if (args[0] === 'yt') {
             let yt = {
                 "title": "Реклама",
@@ -87,41 +76,43 @@ module.exports = {
                   }
                 ]
             }
-            client.channels.cache.get('697352322250178570').send({ embed: yt });
+            message.channel.send({ embed: yt });
+            message.delete();
         } else if (args[0] === 'another') {
             let another = {
                 "title": "Товары",
                 "description": "Товары связанные с майнкрафтом, бравл старс в майнкрафте",
-                 "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
-                 "color": 176128,
-                 "footer": {
-                   "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733689040733208606/pack.png",
-                   "text": "DaironChat"
-                 },
-                 "thumbnail": {
-                   "url": "https://www.minecraft.net/content/dam/minecraft/home/Games_Subnav_Minecraft-228x350.png"
-                 },
-                 "author": {
-                   "name": "Dairon",
-                   "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
-                   "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733684331922456677/download_1.png"
-                 },
-                 "fields": [
-                   {
-                     "name": "Ресурспак Аида с обновлениями",
-                     "value": "**150 руб**\n"
-                   },
-                   {
-                     "name": "Карта Аида с обновлениями",
-                     "value": "**100 руб**\n"
-                   },
-                   {
-                     "name": "Сроки",
-                     "value": "Сразу же после оплаты\n"
-                   }
-                 ]
-            };
-            client.channels.cache.get('697352322250178570').send({ embed: another })
+                "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
+                "color": 176128,
+                "footer": {
+                "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733689040733208606/pack.png",
+                "text": "DaironChat"
+                },
+                "thumbnail": {
+                "url": "https://www.minecraft.net/content/dam/minecraft/home/Games_Subnav_Minecraft-228x350.png"
+                },
+                "author": {
+                "name": "Dairon",
+                "url": "https://www.youtube.com/channel/UCGZhXbVkFCh4pQuzZTuzM2g",
+                "icon_url": "https://cdn.discordapp.com/attachments/730419733672886280/733684331922456677/download_1.png"
+                },
+                "fields": [
+                {
+                    "name": "Ресурспак Аида с обновлениями",
+                    "value": "**150 руб**\n"
+                },
+                {
+                    "name": "Карта Аида с обновлениями",
+                    "value": "**100 руб**\n"
+                },
+                {
+                    "name": "Сроки",
+                    "value": "Сразу же после оплаты\n"
+                }
+                ]
+            }
+            message.channel.send({ embed: another });
+            message.delete();
         };
         
 
