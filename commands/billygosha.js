a22bfa3dc8c0222ac7be8d6a6f65afff27bc4890
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-17 20:51:54 +0300

    Update billygosha.js

--- commands/billygosha.js
+++ commands/billygosha.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'billygosha',
     description: 'bg.',
-    aliases: ['soezbitch', 'vodka', 'fiaysy', 'dboy', 'нек', 'flaysy],
+    aliases: ['soezbitch', 'vodka', 'fiaysy', 'dboy', 'нек'],
 	execute(message, args) {
         
         let images = new Array ('https://vignette.wikia.nocookie.net/peppa-pig/images/2/27/%D0%A1%D0%B2%D0%B8%D0%BD%D0%BA%D0%B0_%D0%9F%D0%B5%D0%BF%D0%BF%D0%B0-0.png/revision/latest/top-crop/width/360/height/450?cb=20190819080228&path-prefix=ru', 'https://cdn.discordapp.com/attachments/692076709301714974/733740581712429226/5WVHOmhzPtY.jpg');
