eec502650c2641af70a26906f7bd0decb0e25d20
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-19 19:27:14 +0300

    Update rsp.js

--- commands/rsp.js
+++ commands/rsp.js
@@ -26,20 +26,33 @@ module.exports = {
                   .setFooter('DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png');
                   message.channel.send(youWin);
         } else {
-            message.channel.send(`Ты не угадал, ответ был ${realAnswer}.`);
+            message.channel.send(`Ты не угадал, ответ был **${realAnswer}**.`);
         };
     };
     if (args[0] == 'ножницы' || args[0] == 'н') {
         if (realAnswer == 'ножницы') {
-            let youWin = new Discord.MessageEmbed()
+            let youWin2 = new Discord.MessageEmbed()
             .setColor('#33FF00')
             .setTitle('Камень Ножницы Бумага')
             .setDescription('**Ты выиграл!**')
             .setDescription(`**Ты** - выбрал **ножницы** :scissors: \n**Dairon (бот)** - выбрал **ножницы** :scissors: `)
             .setFooter('DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png');
-            message.channel.send(youWin);
+            message.channel.send(youWin2);
         } else {
-            message.channel.send(`Ты не угадал, ответ был ${realAnswer}.`);
+            message.channel.send(`Ты не угадал, ответ был **${realAnswer}**.`);
+        };
+    };
+    if (args[0] == 'бумага' || args[0] == 'б') {
+        if (realAnswer == 'ножницы') {
+            let youWin2 = new Discord.MessageEmbed()
+            .setColor('#33FF00')
+            .setTitle('Камень Ножницы Бумага')
+            .setDescription('**Ты выиграл!**')
+            .setDescription(`**Ты** - выбрал **бумага** :newspaper2: \n**Dairon (бот)** - выбрал **бумага** :newspaper2: `)
+            .setFooter('DaironChat','https://cdn.discordapp.com/attachments/698215230114889759/730748859084570674/pack.png');
+            message.channel.send(youWin2);
+        } else {
+            message.channel.send(`Ты не угадал, ответ был **${realAnswer}**.`);
         };
     };
 
