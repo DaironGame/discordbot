6dcb3f3339db4e2dd5fed38207ad564b68a5fd20
DaironGame <68197629+DaironGame@users.noreply.github.com>
2020-07-19 18:52:40 +0300

    Update rsp.js

--- commands/rsp.js
+++ commands/rsp.js
@@ -5,12 +5,11 @@ module.exports = {
         description: 'Погадать.',
         aliases: ['кнб', 'к'],
 	execute(message, args) {
-        let ranColor = "#" + Math.random().toString(16).slice(2, 8);
         let answers = new Array("камень", "ножницы", "бумага");
         let answer = answers[Math.floor(Math.random() * answers.length)];
         if (!args[0]) {
 		 let qEmbed = new Discord.MessageEmbed()
-	      .setColor(ranColor)
+	      .setColor('#33FF00')
 	      .setTitle('Камень Ножницы Бумага')
 		  .setDescription('**Использование:**')
                   .addField('.кнб [камень/ножницы/бумага]', 'Например: `.кнб камень`', true)
@@ -18,14 +17,14 @@ module.exports = {
 		 message.channel.send(qEmbed);
         } else if (answer == 'камень') {
          if (args[0] == 'камень' || args[0] == 'к') {
-             let yourAnswer = 'камень';
              let youWin = new Discord.MessageEmbed()
-             .setColor(ranColor)
+             .setColor('#33FF00')
              .setTitle('Ты выиграл!')
              .setTimestamp
-             .setDescription(`**${message.author.name}** - выбрал ${yourAnswer}\n**Dairon (бот)** - выбрал ${answer}`)
+             .setDescription(`**${message.author.name}** - выбрал камень\n**Dairon (бот)** - выбрал камень`)
              .setFooter('DaironChat');
              message.channel.send(youWin);
+             message.channel.send('камень');
          }
 
 
